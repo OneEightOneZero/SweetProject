7a953fc8cd6ce9d5d924c1c367ca893786d311dc
Bruce lee <1939055535@qq.com>
2019-01-22 15:07:10 +0800

    补充了几个必须依赖

---
+++ src/pages/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script src="../../public/home.html"></script>
+<script src="../styles/style.css"></script>
+<script src="../styles/base.css"></script>
+<script src="../styles/media-queries.css"></script>
+<script>
+export default {
+    
+}
+</script>
